514763b833f04b375fb25ffebedde7b808618c1a
Atemndobs <atemndobs@yahoo.com>
2026-03-23 16:05:26 +0100

    Fix middleware to redirect instead of protect rewrite

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    auth.protect();
+    const authObject = await auth();
+    if (!authObject.userId) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
   }
 });
 
